8665ccaad0a23831ca70aa15a8ccc778c6cac7d2
notquitee <szablewski.mikolaj@gmail.com>
2014-10-16 10:13:36 +0200

    added a comment

--- src/pl/edu/agh/Main.java
+++ src/pl/edu/agh/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	    Box<Integer> integerBox;
         integerBox = new Box<Integer>();
+     //   integerBox.add("10");     //error -> string
         integerBox.add(new Integer(10));
         Integer someInteger = (Integer) integerBox.get();
         System.out.println(someInteger);
